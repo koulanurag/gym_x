923df43e0f42e4d75e159f78db72d1bf222341fb
Anurag Koul <koulanurag@gmail.com>
2018-07-03 23:59:32 -0700

    bug-fix | rename Continuous to Sneak

--- gym_x/__init__.py
+++ gym_x/__init__.py
@@ -4,16 +4,8 @@ from gym.envs.registration import register
 logger = logging.getLogger(__name__)
 
 register(
-    id='GoldRushDiscrete-v0',
-    entry_point='gym_x.envs.gold_rush_discrete:GoldRushD',
-    tags={'wrapper_config.TimeLimit.max_episode_steps': 300,
-          'discrete': True},
-    reward_threshold=300,
-    nondeterministic=False,
-)
-register(
-    id='GoldRushContinuous-v0',
-    entry_point='gym_x.envs.gold_rush_cont:GoldRushC',
+    id='GoldRushSneak-v0',
+    entry_point='gym_x.envs.gold_rush_sneak:GoldRushS',
     tags={'wrapper_config.TimeLimit.max_episode_steps': 300,
           'discrete': False},
     reward_threshold=300,
