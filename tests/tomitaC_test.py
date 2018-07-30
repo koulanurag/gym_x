603f418536ca007b66d1e6ef41f02b9c659c884f
Anurag Koul <koulanurag@gmail.com>
2018-07-29 19:32:35 -0700

    added test case

---
+++ tests/tomitaC_test.py
@@ -0,0 +1,51 @@
+import gym, gym_x
+import os
+import random
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='test env')
+    parser.add_argument('--env', default='GoldRushRead-v0', help="Name of the environment")
+
+    args = parser.parse_args()
+
+    env = gym.make(args.env)
+    env.seed(0)
+    done = False
+    for ep in range(10):
+        done = False
+        obs = env.reset()
+        action = env.env.get_desired_action()
+        total_reward = 0
+        all_observations = [obs]
+        while not done:
+            obs, reward, done, info = env.step(action)
+            action = env.env.get_desired_action()
+            total_reward += reward
+            if not done:
+                all_observations.append(obs)
+        print('Episode: {} Total Reward: {}  Obs: {}'.format(ep, total_reward,
+                                                             ''.join([str(_[0]) for _ in all_observations])))
+        valid = True
+        count = [0, 0]
+        first_one = True
+        last_i = len(all_observations) - 1
+
+        for i, o in enumerate(all_observations):
+            o = o[0]
+            count[o] += 1
+            if first_one and o == 1:
+                first_one = False
+                count = [0, 1]
+            elif (not first_one and o == 1 and i > 0 and all_observations[i - 1] == 0):
+                if (count[1] - 1) % 2 != 0 and count[0] % 2 != 0:
+                    valid = False
+                    break
+                count = [0, 1]
+            elif i == last_i and o == 0:
+                if count[1] % 2 != 0 and count[0] % 2 != 0:
+                    valid = False
+                    break
+                count = [0, 1]
+
+        print(valid)
